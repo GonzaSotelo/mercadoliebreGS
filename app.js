7624365086c9fc6232b096d42b2d45224d3fe28c
GonzaSotelo <g.sotelo.dab@gmail.com>
2023-06-19 19:11:41 -0300

    inicianciando proyecto ML

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3030
+
+const publicPath= path.resolve(__dirname,'./public');
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+app.listen(PORT, () =>{ console.log(`Servidor corriendo en http://localhost:${PORT}`);})
